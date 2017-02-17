cb35677ebdcd04770bb1678f37ebafb9a8e2cd40
Nick ten Veen <nveen0930@gmail.com>
2017-02-17 14:53:48 +0100

    expressed numeric in terms of flatMap

--- entitylang.scala/src/main/scala/org/metaborg/entitylang/analysis/types/typesystem/entitylang/typingrule/package.scala
+++ entitylang.scala/src/main/scala/org/metaborg/entitylang/analysis/types/typesystem/entitylang/typingrule/package.scala
@@ -1,9 +1,10 @@
 package org.metaborg.entitylang.analysis.types.typesystem.entitylang
 
-import org.metaborg.entitylang.analysis.types.typesystem.typingrule.TypingRule
+import org.metaborg.entitylang.analysis.types.typesystem._
+import org.metaborg.entitylang.analysis.types.typesystem.typingrule.{TermTypingRule, TypingRule}
 import org.metaborg.entitylang.analysis.types.{NumericType, Type}
 import org.metaborg.entitylang.lang.ast.MExpression.SExp
 
 package object typingrule {
-  def numeric(e: SExp): TypingRule.Aux[SExp, Type, NumericType] = new NumericTypingRule(e)
+  def numeric(e: SExp)(implicit typeSystem: TypeSystem[SExp, Type]): TermTypingRule[SExp, Type, NumericType] = e.infer.ofType[NumericType]
 }
